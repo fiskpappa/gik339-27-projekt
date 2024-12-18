4bd82cb1b8a9f7fec76afb3a89754807168b6eb4
fiskpappa <h23felno@du.se>
2024-12-18 15:03:11 +0100

    Co-authored-by: llokar <llokar@users.noreply.github.com> Co-authored-by: h23losan <h23losan@users.noreply.github.com> Co-authored-by: Lovisa Wikberg <wikberglovisa@gmail.com>

---
+++ client/script.js
@@ -0,0 +1,98 @@
+const url = 'http://localhost:3000/clients';
+
+/* fetch(url)
+  .then((response) => {
+    return response.json();
+  })
+  .then((clients) => {
+    console.log(clients)
+  }); */
+
+  window.addEventListener('load', fetchData);
+
+function fetchData() {
+  fetch(url)
+    .then((result) => result.json())
+    .then((clients) => {
+      if (clients.length > 0) {
+        let html = `<ul class="col-12">`;
+        clients.forEach((client) => {
+          html += `
+        <li class="clientCard col-3">
+          <h3 class="text-${client.color}-900 text-decoration-none">${client.companyName}</h3>
+          <p>Kontaktperson: ${client.contactName} <br> ${client.contactEmail}</p>
+          <p>Projekttyp: ${client.projectType}</p>
+          <p>Projektlängd: ${client.projectLength} veckor</p>
+          <div>
+            <button class="button" onclick="setCurrentUser(${client.id})">Ändra</button>
+            <button class="button" onclick="deleteUser(${client.id})">Ta bort</button>
+          </div>
+        </li>`;
+        });
+        html += `</ul>`;
+
+        const cardContainer = document.getElementById('cardContainer');
+        cardContainer.innerHTML = '';
+        cardContainer.insertAdjacentHTML('beforeend', html);
+      }
+    });
+}
+
+function setCurrentUser(id) {
+  console.log('current', id);
+
+  fetch(`${url}/${id}`)
+    .then((result) => result.json())
+    .then((client) => {
+      console.log(client);
+      clientForm.companyName.value = client.companyName;
+      clientForm.contactEmail.value = client.contactEmail;
+      clientForm.projectType.value = client.projectType;
+      clientForm.projectLength.value = client.projectLength;
+      clientForm.color.value = client.color;
+      
+      localStorage.setItem('currentId', client.id);
+    });
+}
+
+function deleteUser(id) {
+  console.log('delete', id);
+  fetch(`${url}/${id}`, { method: 'DELETE' }).then((result) => fetchData());
+}
+
+userForm.addEventListener('submit', handleSubmit);
+
+function handleSubmit(e) {
+  e.preventDefault();
+  const serverClientObject = {
+    companyName: '',
+    contactName: '',
+    contactEmail: '',
+    projectType: '',
+    projectLength: '',
+    color: ''
+  };
+  serverClientObject.companyName = clientForm.companyName.value;
+  serverClientObject.contactName = clientForm.contactName.value;
+  serverClientObject.contactEmail = clientForm.contactEmail.value;
+  serverClientObject.projectType = clientForm.projectType.value;
+  serverClientObject.projectLength = clientForm.projectLength
+  serverClientObject.color = clientForm.color.value;
+  const id = localStorage.getItem('currentId');
+  if (id) {
+    serverClientObject.id = id;
+  }
+
+  const request = new Request(url, {
+    method: 'POST',
+    headers: {
+      'content-type': 'application/json'
+    },
+    body: JSON.stringify(serverClientObject)
+  });
+
+  fetch(request).then((response) => {
+    fetchData();
+    clientForm.reset();
+    });
+}
---
+++ server/server.js
@@ -0,0 +1,133 @@
+const express = require('express');
+const server = express();
+const sqlite3 = require('sqlite3').verbose();
+const db = new sqlite3.Database('./client_companies.db');
+
+server
+ .use(express.json())
+ .use(express.urlencoded({ extended: false }))
+ .use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', '*');
+    res.header('Access-Control-Allow-Methods', '*');
+    next();
+ });
+
+ server.listen(3000, () => {
+    console.log('Server running on http://localhost:3000');
+ });
+
+server.get('/clients', (req, res) => {
+    const sql = 'SELECT * FROM clients';
+
+    db.all(sql, (err, rows) => {
+        if (err) {
+            res.status(500).send(err);
+        } else {
+            res.send(rows);
+        }
+    });
+});
+
+server.get('/client/:id', (req, res) => {
+    const id = req.params.id;
+  
+    const sql = `SELECT * FROM users WHERE id=${id}`;
+  
+    db.all(sql, (err, rows) => {
+      if (err) {
+        res.status(500).send(err);
+      } else {
+        res.send(rows[0]);
+      }
+    });
+  });
+
+ 
+server.post('/clients', (req, res) => {
+    const body = req.body;
+    const sql = `INSERT INTO clients(companyName, contactName, contactEmail, projectType, projectLength, color) VALUES (?, ?, ?, ?, ?)`;
+    
+    db.run(sql, Object.values(client), (err) => {
+        if (err) {
+          console.log(err);
+          res.status(500).send(err);
+        } else {
+          res.send("Användaren sparades");
+        }
+      });
+});
+
+
+/* server.put('/clients', (req, res) => {
+    const id = req.params.id;
+    const companyName = req.params.companyName;
+    const contactName = req.params.contactName;
+    const contactEmail = req.params.contactEmail;
+    const projectType = req.params.projectType;
+    const projectLength = req.params.projectLength;
+    const companyColor = req.params.companyColor;
+    const url = req.url;
+    const body = req.body;
+
+    res.send({id, companyName, contactName, contactEmail, projectType,projectLength, color, url, body});
+}); */
+
+server.put('/clients', (req, res) => {
+    const bodyData = req.body;
+  
+    const id = bodyData.id;
+    const client = {
+      companyName: bodyData.companyName,
+      contactName: bodyData.contactName,
+      contactEmail: bodyData.contactEmail,
+      projectType: bodyData.projectType,
+      projectLength: bodyData.projectLength,
+      color: bodyData.color
+    };
+  
+    let updateString = '';
+    const columnsArray = Object.keys(client);
+    columnsArray.forEach((column, i) => {
+      updateString += `${column}="${client[column]}"`;
+      if (i !== columnsArray.length - 1) updateString += ',';
+    });
+    const sql = `UPDATE clients SET ${updateString} WHERE id=${id}`;
+  
+    db.run(sql, (err) => {
+      if (err) {
+        console.log(err);
+        res.status(500).send(err);
+      } else {
+        res.send('Användaren uppdaterades');
+      }
+    });
+
+server.delete('/clients/:id', (req, res) => {
+  const id = req.params.id;
+  const sql = `DELETE FROM clients WHERE id = ${id}`;
+
+  db.run(sql, (err) => {
+    if (err) {
+      console.log(err);
+      res.status(500).send(err);
+    } else {
+      res.send('Användaren borttagen');
+    }
+  });
+});
+
+
+/* server.delete('/clients/:id/:companyName/:contactName/:contactEmail/:projectType/:projectLength/:companyColor', (req, res) => {
+    const id = req.params.id;
+    const sql = `DELETE FROM clients WHERE id = ${id}`;
+
+    db.run(sql, (err) => {
+        if(err) {
+            console.log(err);
+            res.status(500).send(err);
+        } else {
+            res.send('Annons borttagen');
+        }
+    })
+}); */
